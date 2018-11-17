e2aea31ec72ed77e0bf0014576c6654f0dbd3314
Kenny Huynh <khuynh024@gmail.com>
2018-11-17 17:36:35 -0600

    built clock

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+
+function updateTime() {
+
+    const fullDate = new Date();
+    var hours = fullDate.getHours()
+    var minutes = fullDate.getMinutes();
+    var seconds = fullDate.getSeconds();
+
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = ":"+minutes;
+    document.getElementById('second').innerHTML = ":"+seconds;
+}
+
+setInterval(updateTime, 100);
